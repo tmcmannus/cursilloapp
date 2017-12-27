6df519e598cfe1caa0c82ef5d3de276cddaa5095
tmcmannus <artisticre@hotmail.com>
2017-12-26 20:58:38 -0700

    Add files via upload

--- app/Http/Controllers/pilgrimControllers/PilgrimApp.php
+++ app/Http/Controllers/pilgrimControllers/PilgrimApp.php
@@ -16,22 +16,18 @@ class PilgrimApp extends Controller
 
     }
 
-    public function pilgrimappsuccess (Request $request, $sponsor_id)
+    public function pilgrimappsuccess (Request $request, $pilgrimId)
     {
+      $pilgrimInfo = DB::table('pilgrim_info')->where('pilgrim_info.pilgrim_id', $pilgrimId)->first();
+       return view('pilgrim/pilgrimappsuccess')->with(compact('pilgrimInfo'));
 
-
-
-      $success_data = DB::table('pilgrim_info')
-          ->join('sponsor_info', 'pilgrim_info.sponsor_id', 'sponsor_info.sponsor_id')
-          ->select('pilgrim_info.pilgrim_id', 'pilgrim_info.fullname',  'sponsor_info.fullname', $sponsor_id)
-          ->get();
-
-      return view('pilgrim/pilgrimappsuccess')->with(compact('$success_data'));
   }
 
 
-  //$pilgrimInfo = DB::table('pilgrim_info')->where('pilgrim_info.pilgrim_id', $pilgrimId)->first();
-  // return view('pilgrim/pilgrimappsuccess')->with(compact('pilgrimInfo'));
+
+
+
+
 
 
 
