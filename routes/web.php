a20b354c0d221982a219eda0cec3a91fb399f5c3
tmcmannus <artisticre@hotmail.com>
2018-01-04 14:37:36 -0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::prefix('admin')->group(function() {
  Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
  Route::get('/', 'AdminController@index')->name('admin.dashboard');
  Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+ Route::get('/applicationreport', 'AdminController@printApplication');
+
 
 // Password reset routes
   Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
--- storage/framework/views/3f52b25fc4b6b9896c7c3f369dfe215fafe326dc.php
+++ storage/framework/views/3f52b25fc4b6b9896c7c3f369dfe215fafe326dc.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+    <title><?php echo e(config('app.name', 'Lutheran Cursillo of Southern California')); ?></title>
 
     <!-- Styles -->
     <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
@@ -31,7 +31,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
-                        <?php echo e(config('app.name', 'LCSocal')); ?>
+                        <?php echo e(config('app.name', 'Lutheran Cursillo of Southern California')); ?>
 
                     </a>
                 </div>
@@ -78,7 +78,7 @@
         <?php echo $__env->yieldContent('content'); ?>
     </div>
 
-    
+
 
     <!-- Scripts -->
     <script src="<?php echo e(asset('js/app.js')); ?>"></script>
