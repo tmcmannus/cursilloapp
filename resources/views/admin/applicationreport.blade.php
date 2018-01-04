b1fb769aa40317930cbd8a9308d803e9039f3e1c
tmcmannus <artisticre@hotmail.com>
2018-01-04 15:50:53 -0700

    Add files via upload

--- resources/views/admin/applicationreport.blade.php
+++ resources/views/admin/applicationreport.blade.php
@@ -4,4 +4,7 @@
 
                   {{ $application_info->PI_fullname}}<br />
                   {{ $application_info->PI_address}}<br />
+                  {{ $application_info->SP_fullname}}<br />
+                  {{ $application_info->PA_fullname}}<br />
+                  {{ $application_info->PA_churchname}}
 @endsection
