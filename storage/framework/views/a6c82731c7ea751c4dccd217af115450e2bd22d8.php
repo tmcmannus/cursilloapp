1c1211b6bf07f6e570db99872184cda2118b5011
tmcmannus <artisticre@hotmail.com>
2018-01-04 15:52:09 -0700

    Add files via upload

--- storage/framework/views/a6c82731c7ea751c4dccd217af115450e2bd22d8.php
+++ storage/framework/views/a6c82731c7ea751c4dccd217af115450e2bd22d8.php
@@ -2,6 +2,10 @@
 
                   <?php echo e($application_info->PI_fullname); ?><br />
                   <?php echo e($application_info->PI_address); ?><br />
+                  <?php echo e($application_info->SP_fullname); ?><br />
+                  <?php echo e($application_info->PA_fullname); ?><br />
+                  <?php echo e($application_info->PA_churchname); ?>
+
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
